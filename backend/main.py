1528f3238e45294e09c1435942d08d8dfb5c3f6b
Daerian <daerian.dilkumar@hotmail.com>
2026-03-20 15:19:54 -0400

    minor modifications due to python backend swap

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,20 @@
+import os
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from routers import generate
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = FastAPI()
 
+origins = ["http://localhost:5173"]
+if os.getenv("FRONTEND_URL"):
+    origins.append(os.getenv("FRONTEND_URL"))
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=origins,
     allow_methods=["*"],
     allow_headers=["*"],
 )
